8be2290e204e0460035cd86635883ced2a4f975a
Snigdha Tiwari <snigdha2210@gmail.com>
2020-08-18 19:41:41 +0530

    Updates

---
+++ precache-manifest.9de1c2f5f2b378dc283d40763b84f36c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b65a285528ec47cedfa665053e230b01",
+    "url": "/youtube/index.html"
+  },
+  {
+    "revision": "e94d0ec2f46cc9c3e5c0",
+    "url": "/youtube/static/css/main.8ad48a67.chunk.css"
+  },
+  {
+    "revision": "35e5d30d56b02a9f4bad",
+    "url": "/youtube/static/js/2.2605bfb9.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/youtube/static/js/2.2605bfb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e94d0ec2f46cc9c3e5c0",
+    "url": "/youtube/static/js/main.8d46cb95.chunk.js"
+  },
+  {
+    "revision": "2b0ff900b9ec4062d783",
+    "url": "/youtube/static/js/runtime-main.e729373e.js"
+  }
+]);
